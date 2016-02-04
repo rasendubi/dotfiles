3f05fa866caeb4026944c215ca383fbdd3b83372
Alexey Shmalko <rasen.dubi@gmail.com>
2016-02-04 22:06:39 +0200

    Update nvim config
    Neovim now uses XDG directory specification, so nvim configs go to
    .config/nvim directory.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-FILES=".vimrc .vim .nvimrc .nvim .gitconfig .zshrc .zsh .tmux.conf .xxkbrc .Xresources .config/awesome .config/xxkb .nethackrc .emacs.d"
+FILES=".vimrc .vim .nvimrc .nvim .gitconfig .zshrc .zsh .tmux.conf .xxkbrc .Xresources .config/awesome .config/nvim .config/xxkb .nethackrc .emacs.d"
 
 DEST=$1
 
