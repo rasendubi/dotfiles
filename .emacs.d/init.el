09828ac686e60c3c3743f87e946ecbccb7679f0f
Alexey Shmalko <rasen.dubi@gmail.com>
2016-04-08 14:04:13 +0300

    Add rename and delete commands

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -921,6 +921,31 @@ the it takes a second \\[keyboard-quit]] to abort the minibuffer."
   (nmap "SPC f" 'ido-find-file)
   (nmap "SPC b" 'ido-switch-buffer))
 
+;; Shamelessly stealed from from https://github.com/purcell/emacs.d
+(defun rename-this-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (unless filename
+      (error "Buffer '%s' is not visiting file!" name))
+    (if (get-buffer new-name)
+        (message "A buffer named '%s' already exists!" new-name)
+      (progn
+        (when (file-exists-p filename)
+          (rename-file filename new-name 1))
+        (rename-buffer new-name)
+        (set-visited-file-name new-name)))))
+
+(defun delete-this-file ()
+  "Delete the current file, and kill the buffer."
+  (interactive)
+  (or (buffer-file-name) (error "No file is currently being edited"))
+  (when (yes-or-no-p (format "Really delete '%s'?"
+                             (file-name-nondirectory buffer-file-name)))
+    (delete-file (buffer-file-name))
+    (kill-this-buffer)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (c-add-style "rasen"
              '("k&r"
