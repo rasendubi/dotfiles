ebd86e53de1286ddcb17c28102feff122847ff47
Alexey Shmalko <rasen.dubi@gmail.com>
2016-12-28 16:58:31 +0200

    Refactor battery widget

--- .config/awesome/battery.lua
+++ .config/awesome/battery.lua
@@ -17,9 +17,8 @@ end
 
 function battery.get_widget(adapter)
     local battery_widget = wibox.widget.textbox()
-    battery_widget:set_align("right")
 
-    local function battery_info(adapter)
+    local function update()
         local cur = get_file("/sys/class/power_supply/"..adapter.."/charge_now")
         local cap = get_file("/sys/class/power_supply/"..adapter.."/charge_full")
         local sta = get_file("/sys/class/power_supply/"..adapter.."/status")
@@ -30,19 +29,19 @@ function battery.get_widget(adapter)
         local battery = math.floor(cur * 100 / cap)
 
         local function show_info(color, dir)
-            battery_widget:set_markup('<span color="' .. color .. '">' ..
-                                          ' Bat: ' .. dir .. battery .. '%' .. dir .. ' </span>')
+            battery_widget:set_markup('<span color="'..color..'">' ..
+                                          ' Bat: '..dir..battery..'%'..dir..' </span>')
         end
 
         if sta:match("Discharging") then
             if battery < 10 then
-                naughty.notify
-                { title      = "Battery Warning"
-                  , text       = "Battery low! " .. battery .. "% left!"
-                  , timeout    = 5
-                  , position   = "top_right"
-                  , fg         = beautiful.fg_focus
-                  , bg         = beautiful.bg_focus
+                naughty.notify {
+                    title      = "Battery Warning",
+                    text       = "Battery low! " .. battery .. "% left!",
+                    timeout    = 5,
+                    position   = "top_right",
+                    fg         = beautiful.fg_focus,
+                    bg         = beautiful.bg_focus,
                 }
             end
             show_info(battery < 15 and 'red' or 'orange', "↓")
@@ -53,9 +52,9 @@ function battery.get_widget(adapter)
         end
     end
 
-    battery_info(adapter)
-    local battery_timer = timer{timeout = 20}
-    battery_timer:connect_signal("timeout", function() battery_info(adapter) end)
+    update()
+    local battery_timer = timer{timeout = 10}
+    battery_timer:connect_signal("timeout", update)
     battery_timer:start()
 
     return battery_widget
