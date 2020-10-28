a6ca2215eceae64d92d4370a1e8930e62accc766
Alexey Shmalko <rasen.dubi@gmail.com>
2020-10-28 14:43:20 +0200

    [emacs] integrate fish with vterm

--- flake.nix
+++ flake.nix
@@ -556,6 +556,49 @@
             }
             {
               programs.fish = {
+            
+                interactiveShellInit = ''
+                  function vterm_prompt_end;
+                    vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
+                  end
+                  functions --copy fish_prompt vterm_old_fish_prompt
+                  function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
+                    # Remove the trailing newline from the original prompt. This is done
+                    # using the string builtin from fish, but to make sure any escape codes
+                    # are correctly interpreted, use %b for printf.
+                    printf "%b" (string join "\n" (vterm_old_fish_prompt))
+                    vterm_prompt_end
+                  end
+                '';
+            
+                functions.vterm_printf = ''
+                  function vterm_printf;
+                    if [ -n "$TMUX" ]
+                      # tell tmux to pass the escape sequences through
+                      # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+                      printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+                    else if string match -q -- "screen*" "$TERM"
+                      # GNU screen (screen, screen-256color, screen-256color-bce)
+                      printf "\eP\e]%s\007\e\\" "$argv"
+                    else
+                      printf "\e]%s\e\\" "$argv"
+                    end
+                  end
+                '';
+            
+                functions.vterm_cmd = ''
+                  function vterm_cmd --description 'Run an emacs command among the ones been defined in vterm-eval-cmds.'
+                      set -l vterm_elisp ()
+                      for arg in $argv
+                        set -a vterm_elisp (printf '"%s" ' (string replace -a -r '([\\\\"])' '\\\\\\\\$1' $arg))
+                      end
+                      vterm_printf '51;E'(string join "" $vterm_elisp)
+                  end
+                '';
+              };
+            }
+            {
+              programs.fish = {
                 enable = true;
                 shellAliases = {
                   g = "git";
