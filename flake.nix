8dd205d9f6a7e4eadaf8382a163e8994cb11863c
Alexey Shmalko <rasen.dubi@gmail.com>
2021-05-28 08:34:52 +0300

    [direnv] add `use flake` helper

--- flake.nix
+++ flake.nix
@@ -200,6 +200,15 @@
               home.packages = [ pkgs.networkmanagerapplet ];
             }
             {
+              xdg.configFile."direnv/lib/use_flake.sh".text = ''
+                use_flake() {
+                  watch_file flake.nix
+                  watch_file flake.lock
+                  eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
+                }
+              '';
+            }
+            {
               programs.direnv.enable = true;
               services.lorri.enable = true;
             }
