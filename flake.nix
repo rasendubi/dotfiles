cb13d02d948ae3d9e6905598c7db270d27dd0de6
Alexey Shmalko <rasen.dubi@gmail.com>
2021-05-28 10:46:02 +0300

    [nixos] update all inputs

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
         let mkPkgs = system: import inputs.nixpkgs {
               inherit system;
               overlays = self.overlays.${system};
-              config = { allowUnfree = true; };
+              config = { allowUnfree = true; input-fonts.acceptLicense = true; };
             };
         in genAttrs systems mkPkgs;
 
@@ -1029,12 +1029,14 @@
               {
                 # note it's a new attribute and does not override old one
                 input-mono = (pkgs.input-fonts.overrideAttrs (old: {
-                  src = pkgs.requireFile {
-                    name = "Input-Font.zip";
-                    url = "https://input.fontbureau.com/download/index.html?customize&fontSelection=fourStyleFamily&regular=InputMonoNarrow-Regular&italic=InputMonoNarrow-Italic&bold=InputMonoNarrow-Bold&boldItalic=InputMonoNarrow-BoldItalic&a=0&g=0&i=topserif&l=serifs_round&zero=0&asterisk=height&braces=straight&preset=default&line-height=1.2&email=";
-                    sha256 = "0nn41w2b6jvsbr3r4lfy4p8w2ssjmgdjzd1pbj7p0vmawjpvx2w8";
+                  pname = "input-mono";
+                  src = pkgs.fetchurl {
+                    name = "input-mono-${old.version}.zip";
+                    url = "https://input.djr.com/build/?fontSelection=fourStyleFamily&regular=InputMonoNarrow-Regular&italic=InputMonoNarrow-Italic&bold=InputMonoNarrow-Bold&boldItalic=InputMonoNarrow-BoldItalic&a=0&g=0&i=topserif&l=serifs_round&zero=0&asterisk=height&braces=straight&preset=default&line-height=1.2&accept=I+do&email=&.zip";
+                    sha256 = "sha256-VfyVJxh0RY9AXdfE7Xij+ZklGxUvMFfEV1jWoH1h/Q8=";
                   };
-                  outputHash = "1w2i660dg04nyc6fc6r6sd3pw53h8dh8yx4iy6ccpii9gwjl9val";
+                  sourceRoot = ".";
+                  nativeBuildInputs = [ pkgs.unzip ];
                 }));
               }
             ];
