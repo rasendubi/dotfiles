432177fcff51555287a9174f5c4a0b0f29abe86a
Alexey Shmalko <rasen.dubi@gmail.com>
2021-10-20 22:09:50 +0300

    pulseaudio -> pipewire

--- nixos-config.nix
+++ nixos-config.nix
@@ -311,21 +311,47 @@ in
       location.provider = "geoclue2";
     }
     {
-      hardware.pulseaudio = {
+      # PipeWire requires pulseaudio to be disabled.
+      hardware.pulseaudio.enable = false;
+    
+      security.rtkit.enable = true;
+      services.pipewire = {
         enable = true;
-        support32Bit = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+    
+    
+        media-session.config.bluez-monitor.rules = [
+          {
+            # Matches all cards
+            matches = [ { "device.name" = "~bluez_card.*"; } ];
+            actions = {
+              "update-props" = {
+                "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+                # mSBC is not expected to work on all headset + adapter combinations.
+                "bluez5.msbc-support" = true;
+                # SBC-XQ is not expected to work on all headset + adapter combinations.
+                "bluez5.sbc-xq-support" = true;
+              };
+            };
+          }
+          {
+            matches = [
+              # Matches all sources
+              { "node.name" = "~bluez_input.*"; }
+              # Matches all outputs
+              { "node.name" = "~bluez_output.*"; }
+            ];
+            actions = {
+              "node.pause-on-idle" = false;
+            };
+          }
+        ];
       };
     }
     {
       hardware.bluetooth.enable = true;
-      hardware.pulseaudio = {
-        enable = true;
-    
-        # NixOS allows either a lightweight build (default) or full build
-        # of PulseAudio to be installed.  Only the full build has
-        # Bluetooth support, so it must be selected here.
-        package = pkgs.pulseaudioFull;
-      };
     }
     {
       services.udev.packages = [ pkgs.android-udev-rules ];
