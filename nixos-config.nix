cf79afef4919bd6739771b14cdc2ebd7c3a814df
Moritz Schaefer <mail@moritzs.de>
2023-12-09 13:57:47 +0100

    More changes for/with moxps

--- nixos-config.nix
+++ nixos-config.nix
@@ -41,14 +41,13 @@ let
       
         nix.maxJobs = lib.mkDefault 8;
       
-        # TODO enable and check
-        # services.undervolt = {
-        #   enable = true;
-        #   coreOffset = 0;
-        #   gpuOffset = 0;
-        #   # coreOffset = -125;
-        #   # gpuOffset = -75;
-        # };
+        services.undervolt = {
+          enable = true;
+          # coreOffset = 0;
+          # gpuOffset = 0;
+          coreOffset = -125;
+          gpuOffset = -75;
+        };
         powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
         powerManagement.enable = true;
       
@@ -665,27 +664,6 @@ in
       environment.systemPackages = [
         pkgs.home-manager
       ];
-    }
-    {
-      services.hardware.bolt.enable = true;
-      services.udev.extraRules = ''
-        # Remove NVIDIA USB xHCI Host Controller devices, if present
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{remove}="1"
-    
-        # Remove NVIDIA USB Type-C UCSI devices, if present
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{remove}="1"
-    
-        # Remove NVIDIA Audio devices, if present
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{remove}="1"
-    
-        # Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
-        ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
-        ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
-    
-        # Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
-        ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
-        ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
-        '';
     }
     {
       hardware.bluetooth.enable = true;
