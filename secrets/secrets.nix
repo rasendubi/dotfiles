26510204373fb0a9e42b22a4489d4e5717bed2ec
Moritz Schaefer <mail@moritzs.de>
2024-03-10 16:34:29 +0100

    Add client wireguard private key to agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,6 +2,6 @@ let
   user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdJcJ2IxX5QdkZSXiQgCwOnBVsU2oXJCmUmPxUxhixW";
   users = [ user1 ];
   systems = [  ];
-  urls = [ "mcUrl.age" "gcUrl.age" "muwhpc.age" "cemm.age" "wireguard_server_private_key.age" "duckdns_password.age" ];
+  urls = [ "mcUrl.age" "gcUrl.age" "muwhpc.age" "cemm.age" "wireguard_server_private_key.age" "duckdns_password.age" "wireguard_client_private_key.age" ];
 in builtins.listToAttrs (map (url: { name = url; value = { inherit url; publicKeys = users; }; }) urls)
 
