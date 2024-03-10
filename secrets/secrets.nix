87ba1f7f76570f4b5b409225339c4f3a1ecf7437
Moritz Schaefer <mail@moritzs.de>
2024-03-10 15:24:09 +0100

    Move duckdns password to agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,6 +2,6 @@ let
   user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdJcJ2IxX5QdkZSXiQgCwOnBVsU2oXJCmUmPxUxhixW";
   users = [ user1 ];
   systems = [  ];
-  urls = [ "mcUrl.age" "gcUrl.age" "muwhpc.age" "cemm.age" "wireguard_server_private_key.age" ];
+  urls = [ "mcUrl.age" "gcUrl.age" "muwhpc.age" "cemm.age" "wireguard_server_private_key.age" "duckdns_password.age" ];
 in builtins.listToAttrs (map (url: { name = url; value = { inherit url; publicKeys = users; }; }) urls)
 
