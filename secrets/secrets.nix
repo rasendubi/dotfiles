73dbee4b8cddd5a3c640eccaa93db286c156aea4
Moritz Schaefer <mail@moritzs.de>
2024-03-10 15:19:46 +0100

    Set up wireguard for mopad (and add private key to agenix)

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,6 +2,6 @@ let
   user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdJcJ2IxX5QdkZSXiQgCwOnBVsU2oXJCmUmPxUxhixW";
   users = [ user1 ];
   systems = [  ];
-  urls = [ "mcUrl.age" "gcUrl.age" "muwhpc.age" "cemm.age" ];
+  urls = [ "mcUrl.age" "gcUrl.age" "muwhpc.age" "cemm.age" "wireguard_server_private_key.age" ];
 in builtins.listToAttrs (map (url: { name = url; value = { inherit url; publicKeys = users; }; }) urls)
 
